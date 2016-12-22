c709cf52ea7067c93cb0f7a782e5b7740a1ee3d0
softwaregal <agrawal.shruti998@gmail.com>
2016-12-22 18:07:57 +0530

    First commit

---
+++ js1/JScript.js
@@ -0,0 +1,5 @@
+﻿var name=document.getElementById("Text1").innerHtml;
+var age=document.getElementById("Text2").innerHtml;
+var date=document.getElementById("Text3").innerHtml;
+var place_of_birth=document.getElementById("Text2").innerHtml; 
+console.log("name="+name+"\n Age="+age+"\n Date="+date+"\n Place_of_birth="+place_of_birth);
